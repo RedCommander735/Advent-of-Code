d51ae4090b173c5d9db020b887071cc98fd5615d
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-12-03 23:15:34 +0100

    AAAaaaaahhhh

--- day_3/part_1/src/main.rs
+++ day_3/part_1/src/main.rs
@@ -1,5 +1,4 @@
 use std::fs::read_to_string;
-use std::iter::successors;
 use std::{cmp, env};
 
 // try searching special from numerics and then traversing numeric
@@ -24,10 +23,15 @@ fn main() {
                     skip -= 1;
                     continue;
                 }
-                let (return_value, to_subtract) =
-                    check_surrounding(&parsed_array, row_index, char_index);
+                let (return_value, to_subtract) = check_surrounding(
+                    &parsed_array,
+                    row_index.try_into().unwrap(),
+                    char_index.try_into().unwrap(),
+                    (parsed_array.len() - 1).try_into().unwrap(),
+                    (row.len() - 1).try_into().unwrap(),
+                );
 
-                skip = return_value
+                skip = char_index
                     .to_string()
                     .len()
                     .checked_sub(to_subtract)
@@ -43,25 +47,34 @@ fn main() {
     println!("{}", sum);
 }
 
-fn check_surrounding(array: &Vec<Vec<char>>, row_index: usize, char_index: usize) -> (u32, usize) {
-    println!("Indicator: {}", array[row_index][char_index]);
+fn check_surrounding(
+    array: &Vec<Vec<char>>,
+    row_index: isize,
+    char_index: isize,
+    max_row_index: isize,
+    max_char_index: isize,
+) -> (u32, usize) {
+    // println!("Indicator: {}", array[row_index][char_index]);
 
-    let top_row = row_index.checked_sub(1).unwrap_or(0);
-    let bottom_row = cmp::min(row_index + 1, row_index);
-    let left = char_index.checked_sub(1).unwrap_or(0);
-    let right = cmp::min(char_index + 1, char_index);
-    let middle = char_index;
+    let top_row: usize = cmp::max(row_index - 1, 0).try_into().unwrap();
+    let bottom_row: usize = cmp::min(row_index + 1, max_row_index).try_into().unwrap();
+    let left: usize = cmp::max(char_index - 1, 0).try_into().unwrap();
+    let right: usize = cmp::min(char_index + 1, max_char_index).try_into().unwrap();
+    let row_middle: usize = row_index.try_into().unwrap();
+    let middle: usize = char_index.try_into().unwrap();
+
+    println!("{} {} {} {}", top_row, bottom_row, left, right);
 
     if !(array[top_row][left].is_numeric() && array[top_row][left].to_string() == ".")
         || !(array[top_row][middle].is_numeric() && array[top_row][middle].to_string() == ".")
         || !(array[top_row][right].is_numeric() && array[top_row][right].to_string() == ".")
-        || !(array[row_index][left].is_numeric() && array[row_index][left].to_string() == ".")
-        || !(array[row_index][right].is_numeric() && array[row_index][right].to_string() == ".")
+        || !(array[row_middle][left].is_numeric() && array[row_middle][left].to_string() == ".")
+        || !(array[row_middle][right].is_numeric() && array[row_middle][right].to_string() == ".")
         || !(array[bottom_row][left].is_numeric() && array[bottom_row][left].to_string() == ".")
         || !(array[bottom_row][middle].is_numeric() && array[bottom_row][middle].to_string() == ".")
         || !(array[bottom_row][right].is_numeric() && array[bottom_row][right].to_string() == ".")
     {
-        return traverse_number(&array[row_index], char_index);
+        return traverse_number(&array[row_middle], middle.checked_sub(1).unwrap_or(0));
     }
 
     println!("--------------------------------------------------------");
