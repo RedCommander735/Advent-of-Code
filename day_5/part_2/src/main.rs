a6433e71902fdfaffc8ab48fb47aebde0551fbf8
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-12-24 00:13:00 +0100

    Works but slow

--- day_5/part_2/src/main.rs
+++ day_5/part_2/src/main.rs
@@ -1,3 +1,84 @@
+use std::env;
+use std::fs::read_to_string;
+use std::ops::Range;
+
 fn main() {
-    println!("Hello, world!");
+    let mut path = env::current_dir().unwrap();
+    path.push("input.txt");
+
+    let mut output = match path.to_str() {
+        None => panic!("path is not a valid UTF-8 sequence"),
+        Some(s) => parse_file(&s),
+    };
+
+    output.sort();
+
+    println!("\nLowest location number: {}", output.first().unwrap())
+}
+
+fn parse_file(path: &str) -> Vec<i64> {
+    let binding = read_to_string(&path).unwrap();
+    let sections: Vec<&str> = binding.split("\r\n\r\n").collect();
+
+    let mut conversion_maps: Vec<Vec<(Range<i64>, i64)>> = Vec::new();
+
+    for section in &sections {
+        conversion_maps.append(&mut vec![parse_section(section)])
+    }
+
+    let seeds_string: Vec<&str> = sections[0].split(":").collect();
+    let mut seeds: Vec<&str> = seeds_string[1].split(" ").collect();
+    seeds.retain(|&x| x != "");
+
+    let seed_indecies_parsed: Vec<i64> = seeds.iter().map(|x| x.parse::<i64>().unwrap()).collect();
+
+    let mut seeds_parsed: Vec<i64> = Vec::new();
+
+    println!("{:?}", seed_indecies_parsed.len());
+
+    for i in (0..seed_indecies_parsed.len()).step_by(2) {
+        let mut seed_range: Vec<i64> = (seed_indecies_parsed[i]
+            ..seed_indecies_parsed[i] + seed_indecies_parsed[i + 1])
+            .collect();
+        seeds_parsed.append(&mut seed_range)
+    }
+
+    println!("Parsed seeds: {}", seeds_parsed.len());
+
+    let mut parsed_values: Vec<i64> = Vec::new();
+
+    for (index, seed) in seeds_parsed.iter().enumerate() {
+        let mut s: i64 = *seed;
+        print!("\rCurrent seed: {}", index + 1);
+        for range_vec in &conversion_maps {
+            for (range, diff) in range_vec {
+                if range.contains(&s) {
+                    s += diff;
+                    break;
+                }
+            }
+        }
+        parsed_values.append(&mut vec![s]);
+    }
+    println!();
+    parsed_values
+}
+
+fn parse_section(section: &str) -> Vec<(Range<i64>, i64)> {
+    let mut range_vec: Vec<(Range<i64>, i64)> = Vec::new();
+
+    for line in section.lines() {
+        if line.contains(":") {
+            continue;
+        }
+
+        let values: Vec<i64> = line.split(" ").map(|x| x.parse::<i64>().unwrap()).collect();
+        // destination, source, range
+
+        let range = values[1]..values[1] + values[2];
+        let diff = values[0] - values[1];
+
+        range_vec.append(&mut vec![(range, diff)]);
+    }
+    range_vec
 }
