b7e10236ed60e54def5ac75d63097652c2fd3b20
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-12-23 16:55:59 +0100

    Refactor and removed TODO comments

--- day_5/part_1/src/main.rs
+++ day_5/part_1/src/main.rs
@@ -2,18 +2,6 @@ use std::env;
 use std::fs::read_to_string;
 use std::ops::Range;
 
-/*
-create ranges from start to end of source values
-
-check if range .contains value
-
-if range contains value, add difference between source and destination values
-
-done
-
-*/
-
-// TODO: Fix
 fn main() {
     let mut path = env::current_dir().unwrap();
     path.push("input.txt");
@@ -32,10 +20,10 @@ fn parse_file(path: &str) -> Vec<i64> {
     let binding = read_to_string(&path).unwrap();
     let sections: Vec<&str> = binding.split("\r\n\r\n").collect();
 
-    let mut maps: Vec<Vec<(Range<i64>, i64)>> = Vec::new();
+    let mut conversion_maps: Vec<Vec<(Range<i64>, i64)>> = Vec::new();
 
-    for (section_index, section) in sections.iter().enumerate() {
-        maps.append(&mut vec![parse_section(section, section_index)])
+    for section in &sections {
+        conversion_maps.append(&mut vec![parse_section(section)])
     }
 
     let seeds_string: Vec<&str> = sections[0].split(":").collect();
@@ -48,7 +36,7 @@ fn parse_file(path: &str) -> Vec<i64> {
 
     for (index, seed) in seeds_parsed.iter().enumerate() {
         let mut s: i64 = *seed;
-        for range_vec in &maps {
+        for range_vec in &conversion_maps {
             for (range, diff) in range_vec {
                 if range.contains(&s) {
                     s += diff;
@@ -61,7 +49,7 @@ fn parse_file(path: &str) -> Vec<i64> {
     parsed_values
 }
 
-fn parse_section(section: &str, section_index: usize) -> Vec<(Range<i64>, i64)> {
+fn parse_section(section: &str) -> Vec<(Range<i64>, i64)> {
     let mut range_vec: Vec<(Range<i64>, i64)> = Vec::new();
 
     for line in section.lines() {
