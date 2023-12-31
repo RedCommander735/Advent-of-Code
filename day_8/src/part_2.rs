d1358720d812a4e42c599bf8ca04ff3b775060fc
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-12-31 02:08:37 +0100

    part 2 working now

--- day_8/src/part_2.rs
+++ day_8/src/part_2.rs
@@ -1,5 +1,4 @@
-use crate::part_1::nodes_to_map;
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 pub fn part_2(path: &str) -> i32 {
     let binding = read_to_string(&path).unwrap();
@@ -8,19 +7,17 @@ pub fn part_2(path: &str) -> i32 {
     let instructions = lines[0];
     let nodes = &lines[2..];
 
-    let nodes_map = nodes_to_map(nodes.to_vec());
-
-    let mut current_nodes: Vec<&&str> = nodes_map.keys().clone().collect();
+    let (nodes_map, mut current_nodes) = nodes_to_map(nodes.to_vec());
 
     current_nodes.retain(|x| x.ends_with('A'));
 
     let mut counter = 0;
+    for mut node in current_nodes {
+        while !node.ends_with('Z') {
+            for direction in instructions.chars() {
+                counter += 1;
 
-    while !(&current_nodes).iter().all(|x| x.ends_with('Z')) {
-        for direction in instructions.chars() {
-            counter += 1;
-            for mut node in &current_nodes {
-                let intersection = *nodes_map.get(*node).unwrap();
+                let intersection = *nodes_map.get(node).unwrap();
 
                 if direction == 'L' {
                     node = &&intersection.0
@@ -32,4 +29,25 @@ pub fn part_2(path: &str) -> i32 {
     }
 
     counter
+}
+
+fn nodes_to_map(nodes: Vec<&str>) -> (HashMap<&str, (&str, &str)>, Vec<&str>) {
+    let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut nodes_list: Vec<&str> = Vec::new();
+
+    for node in nodes {
+        let content: Vec<&str> = node.split('=').map(|c| c.trim()).collect();
+        let key = content[0];
+
+        let directions: Vec<&str> = content[1]
+            .trim_matches(|d| d == '(' || d == ')')
+            .split(',')
+            .map(|s| s.trim())
+            .collect();
+        let val: (&str, &str) = (directions[0], directions[1]);
+        map.insert(key, val);
+        nodes_list.push(key)
+    }
+
+    (map, nodes_list)
 }
