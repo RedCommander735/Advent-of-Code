12b4b07c5b161ba08a7aaf1ecd91e5050f9d71ef
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-12-31 16:37:24 +0100

    Day 9 part 1 done

---
+++ day_9/src/part_2.rs
@@ -0,0 +1,43 @@
+use std::fs::read_to_string;
+
+pub fn main(path: &str) -> i32 {
+    let binding = read_to_string(&path).unwrap();
+    let lines: Vec<&str> = binding.lines().collect();
+
+    let mut next_values = Vec::<i32>::new();
+
+    for line in lines {
+        next_values.push(extrapolate_prev_value(line));
+    }
+
+    next_values.iter().sum()
+}
+
+fn extrapolate_prev_value(line: &str) -> i32 {
+    let mut sequence: Vec<i32> = line.split(' ').map(|n| n.parse::<i32>().unwrap()).collect();
+
+    let mut sequences: Vec<Vec<i32>> = Vec::new();
+    sequences.push(sequence.clone());
+
+    while !sequence.iter().all(|n| *n == 0) {
+        let mut diff: Vec<i32> = Vec::new();
+
+        for index in 1..sequence.len() {
+            diff.push(sequence[index] - sequence[index - 1]);
+        }
+
+        sequences.push(diff.clone());
+        sequence = diff;
+    }
+
+    sequences.pop();
+    sequences.reverse();
+
+    let mut last_diff = 0;
+
+    for seq in &sequences {
+        last_diff -= seq.first().unwrap()
+    }
+
+    last_diff
+}
