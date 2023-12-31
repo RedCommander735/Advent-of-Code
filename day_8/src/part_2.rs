c1d4ec5e402c5f96cb8fe6078010ecabb6c95bb7
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2023-12-31 02:08:17 +0100

    Revert "nothing working"
    This reverts commit ea770ca70a8cf75461767cba5301447f312e96bb.

--- day_8/src/part_2.rs
+++ day_8/src/part_2.rs
@@ -16,34 +16,19 @@ pub fn part_2(path: &str) -> i32 {
 
     let mut counter = 0;
 
-    while !current_nodes.iter().all(|x| x.ends_with('Z')) {
-        let mut new_nodes = Vec::new();
+    while !(&current_nodes).iter().all(|x| x.ends_with('Z')) {
+        for direction in instructions.chars() {
+            counter += 1;
+            for mut node in &current_nodes {
+                let intersection = *nodes_map.get(*node).unwrap();
 
-        for index in 0..current_nodes.len() {
-            let node = &current_nodes[index];
-            let intersection = *nodes_map.get(*node).unwrap();
-
-            for direction in instructions.chars() {
-                let next_node = if direction == 'L' {
-                    &intersection.0
+                if direction == 'L' {
+                    node = &&intersection.0
                 } else {
-                    &intersection.1
-                };
-
-                // Assuming ends_with('Z') check needs to be done here
-                if next_node.ends_with('Z') {
-                    // Handle the case when the node ends with 'Z'
+                    node = &&intersection.1
                 }
-
-                new_nodes.push(next_node.to_string());
             }
         }
-
-        let new_new_nodes: Vec<&&str> = new_nodes.iter().map(|node| node.as_str()).collect();
-
-        // Update current_nodes with the new values
-        current_nodes = new_new_nodes;
-        counter += 1;
     }
 
     counter
